d3a5410e16695b2e2b4dc72788cc9836439423e6
meganii <yuhei24@gmail.com>
2017-06-25 23:17:08 +0900

    Update

--- stocks/google_finance.rb
+++ stocks/google_finance.rb
@@ -1,39 +1,69 @@
 require 'faraday'
 require 'date'
 
-params = {
-  q: 3237,   # code
-  i: 86_400, # interval
-  x: 'TYO',  # market
-  p: '1Y',   # period
-  ts: DateTime.now.to_datetime # lastdate
-}
+class GoogleFinance
+  def get_price(code, market, period, lastdate, interval = 86_400)
+    response = prices_response(code, market, period, lastdate, interval)
+    lines = response.body.each_line.map(&:chomp)
+    columns = lines[4].split('=')[1].split(',')
+    prices = lines.slice(8..lines.size)
+    calc_date(prices, columns)
+  end
 
-conn = Faraday.new(url: 'https://www.google.com/finance/getprices') do |builder|
-  builder.request  :url_encoded
-  builder.adapter  :net_http
+  def prices_response(code, market, period, lastdate, interval = 86_400)
+    params = {
+      q:  code,     # code
+      i:  interval, # interval
+      x:  market,   # market
+      p:  period,   # period
+      ts: lastdate  # lastdate
+    }
+
+    conn = Faraday.new(url: 'https://www.google.com/finance/getprices') do |builder|
+      builder.request  :url_encoded
+      builder.adapter  :net_http
+    end
+
+    response = conn.get { |req| req.params = params }
+    response
+  end
+
+  def get_current_price(code, market)
+    prices = get_price(code, market, '1Y', DateTime.now.to_datetime, 86_400)
+    prices[prices.size - 1][1]
+  end
+
+  private
+
+  def calc_date(prices, columns)
+    base_date = base_date(prices)
+    fst_cols = prices[0].split(',')
+
+    result = []
+    result << columns
+    result << [base_date.strftime('%Y-%m-%d'), fst_cols[1], fst_cols[2], fst_cols[3], fst_cols[4], fst_cols[5]]
+
+    prices.slice(1..prices.size).each do |price|
+      cols = price.split(',')
+      date = base_date + cols[0].to_i
+      result << [date.strftime('%Y-%m-%d'), cols[1], cols[2], cols[3], cols[4], cols[5]]
+    end
+    result
+  end
+
+  def base_date(prices)
+    first_col = prices[0].split(',')
+    base_date = Time.at(first_col[0].delete('a').to_i)
+    Date.parse(base_date.strftime('%Y-%m-%d'), '%Y-%m-%d')
+  end
 end
 
-response = conn.get do |req|
-  req.params = params
+gf = GoogleFinance.new
+prices = gf.get_price('3237', 'TYO', '1Y', DateTime.now.to_datetime)
+
+prices.each do |price|
+  p price
 end
 
-lines = response.body.each_line.map(&:chomp)
-columns = lines[4].split('=')[1].split(',')
-prices = lines.slice(8..lines.size)
-
-fst_cols = prices[0].split(',')
-timestamp = fst_cols[0].delete('a')
-first_date = Time.at(timestamp.to_i)
-
-result = []
-result << columns
-result << [first_date.strftime('%Y-%m-%d'),
-           fst_cols[1], fst_cols[2], fst_cols[3], fst_cols[4], fst_cols[5]]
-
-prices.slice(1..prices.size).each do |price|
-  cols = price.split(',')
-  date = Date.parse(first_date.strftime('%Y-%m-%d'), '%Y-%m-%d') + cols[0].to_i
-  result << [date.strftime('%Y-%m-%d'), cols[1],
-             cols[2], cols[3], cols[4], cols[5]]
-end
+current_price = gf.get_current_price('3237', 'TYO')
+p "current_price: #{current_price}"
